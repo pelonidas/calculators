1c767c1c560d8ba9117b368c4ac46d4868f5edd4
pelonidas <janx803y@easv365.dk>
2022-06-20 17:34:29 +0200

    Neviem

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,9 +1,11 @@
 import {useState} from "react";
 
-export default (initValue = '') => {
+const useInput = (initValue = '') => {
     const [val, setVal] = useState(initValue)
     const handleChange = (e) => {
         setVal(e.target.value)
     }
     return [val, handleChange]
  }
+
+ export default useInput;
